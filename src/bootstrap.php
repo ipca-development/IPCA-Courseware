92bcdde6fdea92d5c39bbd056d4093b2bbc4353b
ipca-development <kay.vereeken@me.com>
2026-02-23 11:42:49 -0800

    Update bootstrap.php

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 require_once __DIR__ . '/helpers.php';
 require_once __DIR__ . '/db.php';
 require_once __DIR__ . '/auth.php';
+require_once __DIR__ . '/render.php';
 
 session_start();
 
