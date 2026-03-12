20802ec1725ab2e1579769de8038dfc4f21563e2
ipca-development <kay.vereeken@me.com>
2026-03-11 18:37:15 -0700

    Uodate before moving up to the faster AI

---
+++ public/php_version_check.php
@@ -0,0 +1,7 @@
+<?php
+require_once __DIR__ . '/../src/bootstrap.php';
+
+header('Content-Type: text/plain; charset=utf-8');
+
+echo 'PHP_VERSION=' . PHP_VERSION . "\n";
+echo 'PHP_SAPI=' . PHP_SAPI . "\n";
