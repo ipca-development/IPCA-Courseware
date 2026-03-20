23283c0a6a90e9a06cd9e57467c27247f41c2e39
ipca-development <kay.vereeken@me.com>
2026-03-20 14:44:58 -0700

    Update edit.php

--- public/admin/users/edit.php
+++ public/admin/users/edit.php
@@ -516,9 +516,7 @@ cw_header('User Workspace');
                     <?php foreach ($missingFields as $missingItem): ?>
                         <a class="ue-list-item ue-list-item-link" href="<?php echo h(aue_missing_field_url($userId, $missingItem)); ?>">
                             <div class="ue-list-title"><?php echo h((string)($missingItem['label'] ?? 'Unknown field')); ?></div>
-                            <a class="ue-list-item ue-list-item-link" href="<?php echo h(aue_missing_field_url($userId, (string)$field)); ?>">
-    							<div class="ue-list-title"><?php echo h((string)$field); ?></div>
-							</a>
+                            
                         </a>
                     <?php endforeach; ?>
                 </div>
