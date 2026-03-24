f01e5675a344117068fb0efafceb4bc02aca0a39
ipca-development <kay.vereeken@me.com>
2026-03-24 12:43:02 -0700

    Update bulk_enrich_run.php

--- public/admin/api/bulk_enrich_run.php
+++ public/admin/api/bulk_enrich_run.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/../../../src/bootstrap.php';
-//require_once __DIR__ . '/../../../src/openai.php';
+require_once __DIR__ . '/../../../src/openai.php';
 
 echo "START<br>";
 flush();
