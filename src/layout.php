9cbd06490fa3558c2e523831a66984c02a14bc2f
ipca-development <kay.vereeken@me.com>
2026-03-24 08:00:12 -0700

    Update layout.php

--- src/layout.php
+++ src/layout.php
@@ -53,6 +53,7 @@ function cw_header(string $title = ''): void
 <link rel="icon" href="/favicon.ico" sizes="any">
 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="apple-touch-icon" href="/favicon-180x180.png">
 <link rel="apple-touch-icon" sizes="180x180" href="/favicon-180x180.png">
 <link rel="manifest" href="/site.webmanifest">
 
