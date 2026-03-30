efc0546eb0e308cb7f9ac9aac68373bcfb70be1e
ipca-development <kay.vereeken@me.com>
2026-03-30 13:15:37 -0700

    testing

--- public/admin/api/ai_jake_console_action.php
+++ public/admin/api/ai_jake_console_action.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 
 declare(strict_types=1);
 
-require_once __DIR__ . '/../../../../src/bootstrap.php';
+require_once __DIR__ . '/../../../src/bootstrap.php';
 
 header('Content-Type: application/json; charset=utf-8');
 
--- public/admin/api/ai_tester.php
+++ public/admin/api/ai_tester.php
@@ -7,7 +7,7 @@ error_reporting(E_ALL);
 
 echo "STEP 1<br>";
 
-require_once __DIR__ . '/../../../../src/bootstrap.php';
+require_once __DIR__ . '/../../../src/bootstrap.php';
 
 echo "STEP 2<br>";
 
