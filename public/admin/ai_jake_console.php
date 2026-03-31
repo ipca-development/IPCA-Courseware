cd322ff4fdd48706bb55bb795c977aa07e96ee09
ipca-development <kay.vereeken@me.com>
2026-03-31 16:45:14 -0700

    Update ai_jake_console.php

--- public/admin/ai_jake_console.php
+++ public/admin/ai_jake_console.php
@@ -26,7 +26,9 @@ try {
     $recentArtifacts = array();
 }
 
-<?php cw_header('Jake Console'); ?>
+
+cw_header('Jake Console');
+?>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/themes/prism-tomorrow.min.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/components/prism-core.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/components/prism-clike.min.js"></script>
@@ -38,6 +40,7 @@ try {
 <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/components/prism-php.min.js"></script>
 
 <style>
+
   .jake-shell{
     display:flex;
     flex-direction:column;
