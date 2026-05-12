589e495166b7d33db0340272bfcbb6c0465b12a8
ipca-development <kay.vereeken@me.com>
2026-05-11 18:59:12 -0700

    strange and suspicious drifty update

--- public/register.php
+++ public/register.php
@@ -318,6 +318,7 @@ $insertUser->execute(array(
 <title>Register · IPCA Academy</title>
 <meta name="theme-color" content="#1e3c72">
 <meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="mobile-web-app-capable" content="yes">
 <meta name="apple-mobile-web-app-status-bar-style" content="default">
 <meta name="apple-mobile-web-app-title" content="IPCA Academy">
 
